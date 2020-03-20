5952e529a09666a5db4141129ef7502a0e2d5dd7
CompulsiveCoder <compulsivecoder@gmail.com>
2020-03-21 09:01:49 +1100

    Added systemctl daemon reload and console output to reinstall script

--- reinstall.sh
+++ reinstall.sh
@@ -1,7 +1,19 @@
 echo "Reinstalling GrowSense..."
 
+echo ""
+echo "  Uninstalling..."
+echo ""
 bash uninstall.sh || exit 1
 
+echo ""
+echo "  Installing..."
+echo ""
 bash install.sh || exit 1
 
+echo ""
+echo "  Reloading systemctl daemon..."
+echo ""
+sudo systemctl daemon-reload
+
+echo ""
 echo "Reinstall complete."
